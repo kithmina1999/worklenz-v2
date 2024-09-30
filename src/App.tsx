5cacfe277daba97c055c59ca96b74a70aab2d9b5
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-09-30 16:55:49 +0530

    feat(app): add routes for (worklenz/*) main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
-import { createBrowserRouter, createRoutesFromElements, Navigate, Route, RouterProvider } from 'react-router-dom'
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Navigate,
+    Route,
+    RouterProvider,
+} from 'react-router-dom'
 
 import LoginPage from './pages/auth/LoginPage'
 import SignupPage from './pages/auth/SignupPage'
@@ -7,6 +13,8 @@ import AuthLayout from './layouts/AuthLayout'
 import ThemeWrapper from './features/theme/ThemeWrapper'
 import PreferenceSelector from './components/PreferenceSelector'
 import NotFoundPage from './pages/NotFoundPage'
+import MainLayout from './layouts/MainLayout'
+import Homepage from './pages/home/Homepage'
 
 const App = () => {
     const router = createBrowserRouter(
@@ -25,10 +33,15 @@ const App = () => {
                     />
                 </Route>
 
+                {/* main pages routes */}
+                <Route path="/worklenz" element={<MainLayout />}>
+                    <Route path="/worklenz/home" element={<Homepage />} />
+                </Route>
+
                 {/* not found pages */}
                 <Route path="*" element={<NotFoundPage />} />
-            </Route>,
-        ),
+            </Route>
+        )
     )
     return (
         <ThemeWrapper>
