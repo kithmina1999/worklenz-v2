8073b2ea2c38ee34896791d1f3426ab4655109c8
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-10-01 17:16:49 +0530

    feat(createProject): add button to open project drawer

---
+++ src/features/projects/createProject/CreateProjectButton.tsx
@@ -0,0 +1,33 @@
+import { Dropdown } from 'antd'
+import React from 'react'
+import { useAppDispatch } from '../../../hooks/useAppDispatch'
+import { toggleDrawer } from './createProjectSlice'
+import { DownOutlined, EditOutlined, ImportOutlined } from '@ant-design/icons'
+
+const CreateProjectButton = () => {
+    const dispatch = useAppDispatch()
+
+    const items = [
+        {
+            key: '1',
+            label: (
+                <div>
+                    <ImportOutlined /> Import from template
+                </div>
+            ),
+        },
+    ]
+
+    return (
+        <Dropdown.Button
+            type="primary"
+            icon={<DownOutlined />}
+            onClick={() => dispatch(toggleDrawer())}
+            menu={{ items }}
+        >
+            <EditOutlined /> Create Project
+        </Dropdown.Button>
+    )
+}
+
+export default CreateProjectButton
