8e25a1061dd5b885e8cba95abf54e2c93020844e
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-10-01 17:20:39 +0530

    feat(layouts): add navbar to main layout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,10 +1,28 @@
 import { Col, Layout } from 'antd'
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from '../components/navbar/Navbar'
+import { useAppSelector } from '../hooks/useAppSelector'
 
 const MainLayout = () => {
+    const themeMode = useAppSelector((state) => state.themeReducer.mode)
+
     return (
         <Layout>
+            <Layout.Header
+                className={`shadow-md ${themeMode === 'dark' ? 'shadow-[#5f5f5f1f]' : 'shadow-[#18181811]'}`}
+                style={{
+                    zIndex: 999,
+                    position: 'fixed',
+                    width: '100%',
+                    display: 'flex',
+                    alignItems: 'center',
+                    padding: 0,
+                }}
+            >
+                <Navbar />
+            </Layout.Header>
+
             <Layout.Content>
                 <Col
                     xs={{ span: 20, offset: 2, flex: '100%' }}
