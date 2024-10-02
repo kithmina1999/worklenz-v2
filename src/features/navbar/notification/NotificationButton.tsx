d5c4c3f2a199ce35a22738900d1c1814c41100aa
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-10-02 17:34:44 +0530

    feat(navbar): abb button with notification icon for open notification drawer

---
+++ src/features/navbar/notification/NotificationButton.tsx
@@ -0,0 +1,25 @@
+import { BellOutlined } from '@ant-design/icons'
+import { Button, Tooltip } from 'antd'
+import React from 'react'
+import { toggleDrawer } from './notificationSlice'
+import { useAppDispatch } from '../../../hooks/useAppDispatch'
+import { useTranslation } from 'react-i18next'
+
+const NotificationButton = () => {
+    const dispatch = useAppDispatch()
+
+    // localization
+    const { t } = useTranslation('navbar')
+
+    return (
+        <Tooltip title={t('notificationTooltip')} trigger={'hover'}>
+            <Button
+                className="navbar-action-btn"
+                icon={<BellOutlined style={{ fontSize: 20 }} />}
+                onClick={() => dispatch(toggleDrawer())}
+            />
+        </Tooltip>
+    )
+}
+
+export default NotificationButton
