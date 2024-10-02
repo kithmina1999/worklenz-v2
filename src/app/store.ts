2d2e381bbe6b93b5459bbce9af983e5186c28e51
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-10-02 17:20:36 +0530

    feat(store): add notification, add member, task reducers

--- src/app/store.ts
+++ src/app/store.ts
@@ -4,14 +4,24 @@ import localesReducer from '../features/i18n/localesSlice'
 import todoReducer from '../features/todo/todoSlice'
 import dateReducer from '../features/date/dateSlice'
 import createProjectReducer from '../features/projects/createProject/createProjectSlice'
+import notificationReducer from '../features/navbar/notification/notificationSlice'
+import addMemberReducer from '../features/navbar/addMember/addMemberSlice'
+import taskReducer from '../features/tasks/taskSlice'
 
 export const store = configureStore({
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
     reducer: {
         themeReducer: themeReducer,
         localesReducer: localesReducer,
         todoReducer: todoReducer,
         dateReducer: dateReducer,
         createProjectReducer: createProjectReducer,
+        notificationReducer: notificationReducer,
+        addMemberReducer: addMemberReducer,
+        taskReducer: taskReducer,
     },
 })
 
