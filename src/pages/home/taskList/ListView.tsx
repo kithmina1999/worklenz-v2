64b764ebbf3831db80b52b3053e1ff988523a841
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-10-04 16:55:54 +0530

    refactor(homepage-tasklist): update list view table

--- src/pages/home/taskList/ListView.tsx
+++ src/pages/home/taskList/ListView.tsx
@@ -1,10 +1,14 @@
-import { Input, Table, Tabs } from 'antd'
+import { Select, Table, TableProps, Tabs } from 'antd'
 import React from 'react'
 import EmptyListPlaceholder from '../../../components/EmptyListPlaceholder'
 import AddTaskInlineForm from './AddTaskInlineForm'
+import { useAppSelector } from '../../../hooks/useAppSelector'
+import { TaskType } from '../../../types/task'
 
 const ListView = () => {
-    const tasksList: string[] = []
+    const tasksList = useAppSelector((state) => state.taskReducer.tasks)
+
+    console.log(tasksList)
 
     // tasks filter tab items
     const tabItems = [
@@ -31,6 +35,30 @@ const ListView = () => {
         },
     ]
 
+    const statusOptions = [
+        { value: 'todo', label: 'Todo' },
+        { value: 'doing', label: 'Doing' },
+        { value: 'done', label: 'Done' },
+    ]
+
+    // table columns
+    const columns: TableProps<TaskType>['columns'] = [
+        { key: 'task', title: 'Task', dataIndex: 'task' },
+        { key: 'project', title: 'Project', dataIndex: 'project' },
+        {
+            key: 'status',
+            title: 'Status',
+            render: (values) => (
+                <Select
+                    defaultValue={values.status.toLowerCase()}
+                    options={statusOptions}
+                    variant="borderless"
+                />
+            ),
+        },
+        { key: 'dueDate', title: 'Due Date', dataIndex: 'dueDate' },
+    ]
+
     return (
         <div>
             {/* tasks filtering tabs */}
@@ -41,7 +69,11 @@ const ListView = () => {
                     text=" No tasks to show."
                 />
             ) : (
-                <Table dataSource={tasksList}></Table>
+                <Table
+                    columns={columns}
+                    dataSource={tasksList}
+                    rowKey={(record) => record.taskId}
+                />
             )}
         </div>
     )
