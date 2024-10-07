1b7d07580f86a796667defa99830cc454e0c5b94
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-10-07 17:10:48 +0530

    refactor(navbar): update switch team button

--- src/features/navbar/switchTeam/SwitchTeamButton.tsx
+++ src/features/navbar/switchTeam/SwitchTeamButton.tsx
@@ -1,20 +1,14 @@
 import {
     BankOutlined,
-    CheckCircleTwoTone,
-    DownOutlined,
+    CaretDownFilled,
+    CheckCircleFilled,
 } from '@ant-design/icons'
-import {
-    Avatar,
-    Button,
-    Dropdown,
-    Flex,
-    Space,
-    Tooltip,
-    Typography,
-} from 'antd'
+import { Avatar, Card, Dropdown, Flex, Tooltip, Typography } from 'antd'
 import React from 'react'
 import { colors } from '../../../styles/colors'
 import { useTranslation } from 'react-i18next'
+// custom css
+import './switchTeam.css'
 
 const SwitchTeamButton = () => {
     // localization
@@ -25,45 +19,74 @@ const SwitchTeamButton = () => {
         {
             key: '0',
             label: (
-                <Space>
-                    <Avatar
-                        size="small"
-                        style={{ backgroundColor: colors.vibrantOrange }}
-                    >
-                        S
-                    </Avatar>
-                    <Flex vertical>
-                        <Typography.Text
-                            style={{ fontSize: 12, fontWeight: 300 }}
-                        >
-                            owned by you
-                        </Typography.Text>
-                        <Typography.Text>Sachintha Prasad</Typography.Text>
+                <Card
+                    className="switch-team-card"
+                    bordered={false}
+                    style={{
+                        width: 230,
+                    }}
+                >
+                    <Flex vertical gap={8}>
+                        <Flex gap={12} align="center" style={{ padding: 12 }}>
+                            <Avatar
+                                style={{
+                                    backgroundColor: colors.vibrantOrange,
+                                }}
+                            >
+                                S
+                            </Avatar>
+                            <Flex vertical>
+                                <Typography.Text
+                                    style={{
+                                        fontSize: 12,
+                                        fontWeight: 300,
+                                        color: colors.lightGray,
+                                    }}
+                                >
+                                    Owned by you
+                                </Typography.Text>
+                                <Typography.Text>
+                                    Sachintha Prasad
+                                </Typography.Text>
+                            </Flex>
+                            <CheckCircleFilled
+                                style={{
+                                    fontSize: 16,
+                                    color: colors.limeGreen,
+                                }}
+                            />
+                        </Flex>
                     </Flex>
-                    <CheckCircleTwoTone twoToneColor={colors.limeGreen} />
-                </Space>
+                </Card>
             ),
         },
     ]
 
     return (
-        <Tooltip title={t('switchTeamTooltip')}>
-            <Dropdown menu={{ items }} trigger={['click']}>
-                <Button
-                    type="text"
-                    shape="round"
-                    size="large"
-                    icon={<BankOutlined />}
+        <Tooltip title={t('switchTeamTooltip')} trigger={'hover'}>
+            <Dropdown
+                overlayClassName="switch-team-dropdown"
+                menu={{ items }}
+                trigger={['click']}
+            >
+                <Flex
+                    gap={4}
+                    align="center"
+                    justify="center"
                     style={{
                         color: colors.skyBlue,
                         backgroundColor: colors.paleBlue,
-                        border: 'none',
                         fontWeight: 500,
+                        borderRadius: '50rem',
+                        padding: '10px 16px',
                     }}
                 >
-                    Sachintha Prasad
-                    <DownOutlined />
-                </Button>
+                    <BankOutlined />
+                    <Typography.Text strong style={{ color: colors.skyBlue }}>
+                        Sachintha Prasad
+                    </Typography.Text>
+                    <CaretDownFilled />
+                </Flex>
             </Dropdown>
         </Tooltip>
     )
