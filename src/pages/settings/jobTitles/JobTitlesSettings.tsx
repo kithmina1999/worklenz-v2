2aea9822fb45f8bb2e13f569a761f8acaf733249
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-10-09 15:51:02 +0530

    refactor(settings): update settings components

--- src/pages/settings/jobTitles/JobTitlesSettings.tsx
+++ src/pages/settings/jobTitles/JobTitlesSettings.tsx
@@ -1,7 +1,58 @@
+import { PushpinOutlined } from '@ant-design/icons'
+import { Button, Card, Flex, Input, Table, TableProps, Tooltip } from 'antd'
 import React from 'react'
+import { colors } from '../../../styles/colors'
 
 const JobTitlesSettings = () => {
-    return <div>JobTitlesSettings</div>
+    // table columns
+    const columns: TableProps['columns'] = [
+        {
+            key: 'name',
+            title: 'Name',
+            dataIndex: 'name',
+            sorter: (a, b) => a.name.length - b.name.length,
+        },
+    ]
+
+    return (
+        <Card
+            style={{ width: '100%' }}
+            title={
+                <Flex justify="flex-end">
+                    <Flex
+                        gap={8}
+                        align="center"
+                        justify="flex-end"
+                        style={{ width: '100%', maxWidth: 400 }}
+                    >
+                        <Input.Search
+                            placeholder="Search by name"
+                            style={{ maxWidth: 200 }}
+                        />
+                        <Button type="primary">Create Job Title</Button>
+                        <Tooltip
+                            title={'Click to pin this into the main menu'}
+                            trigger={'hover'}
+                        >
+                            <Button
+                                className="borderless-icon-btn"
+                                icon={
+                                    <PushpinOutlined
+                                        style={{
+                                            fontSize: 18,
+                                            color: colors.skyBlue,
+                                        }}
+                                    />
+                                }
+                            />
+                        </Tooltip>
+                    </Flex>
+                </Flex>
+            }
+        >
+            <Table columns={columns} />
+        </Card>
+    )
 }
 
 export default JobTitlesSettings
--- src/pages/settings/projectTemplates/ProjectTemplatesSettings.tsx
+++ src/pages/settings/projectTemplates/ProjectTemplatesSettings.tsx
@@ -1,7 +1,21 @@
+import { Card, Table, TableProps } from 'antd'
 import React from 'react'
 
 const ProjectTemplatesSettings = () => {
-    return <div>ProjectTemplatesSettings</div>
+    // table columns
+    const columns: TableProps['columns'] = [
+        {
+            key: 'name',
+            title: 'Name',
+            dataIndex: 'name',
+        },
+    ]
+
+    return (
+        <Card style={{ width: '100%' }}>
+            <Table columns={columns} />
+        </Card>
+    )
 }
 
 export default ProjectTemplatesSettings
