a1ac18399749694c78b912af76a08a3d8b3018a7
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-10-18 16:53:48 +0530

    feat(hooks): custom hook for get the currently selected project object

---
+++ src/hooks/useSelectedProject.ts
@@ -0,0 +1,21 @@
+import { useParams } from 'react-router-dom'
+import { useAppSelector } from './useAppSelector'
+
+// this custom hook return currently selected project
+export const useSelectedProject = () => {
+    const { projectId } = useParams()
+
+    const projectList = useAppSelector(
+        (state) => state.projectReducer.projectsList
+    )
+
+    const selectedProject = projectList.find(
+        (project) => project.projectId === projectId
+    )
+
+    try {
+        return selectedProject
+    } catch (error) {
+        console.error('custom error: error in selecting a project')
+    }
+}
