3c31b702f520a9c66e5cef8a9db812da6f56f07c
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-10-18 17:00:12 +0530

    feat(project): add invite button and drawer to project view

---
+++ src/features/projects/singleProject/members/ProjectMemberInviteButton.tsx
@@ -0,0 +1,21 @@
+import { UsergroupAddOutlined } from '@ant-design/icons'
+import { Button } from 'antd'
+import React from 'react'
+import { useAppDispatch } from '../../../../hooks/useAppDispatch'
+import { toggleDrawer } from './projectMembersSlice'
+
+const ProjectMemberInviteButton = () => {
+    const dispatch = useAppDispatch()
+
+    return (
+        <Button
+            type="primary"
+            icon={<UsergroupAddOutlined />}
+            onClick={() => dispatch(toggleDrawer())}
+        >
+            Invite
+        </Button>
+    )
+}
+
+export default ProjectMemberInviteButton
