10bf22225e608137f16765ef1219df7c4dd1b69f
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-10-24 00:36:06 +0530

    chore(types): update task type

--- src/types/task.types.ts
+++ src/types/task.types.ts
@@ -1,11 +1,25 @@
 import { ProjectType } from './project.types'
 
-export type TaskStatusType = 'Doing' | 'Todo' | 'Done'
+export type TaskStatusType = 'doing' | 'todo' | 'done'
+export type TaskPriorityType = 'low' | 'medium' | 'high'
 
 export type TaskType = {
     taskId: string
     task: string
+    description?: string
+    progress?: string
+    members?: string
+    labels?: string
     status: TaskStatusType
-    dueDate: string
-    project: ProjectType
+    priority: TaskPriorityType
+    timeTracking?: string
+    estimation?: string
+    startDate?: Date
+    dueDate?: Date
+    completedDate?: Date
+    createdDate?: Date
+    lastUpdated?: Date
+    reporter?: string
+    phase?: string
+    project?: ProjectType
 }
