eda58a7761bd1bca7bedbbd019d5cde79cc7144d
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-10-24 00:35:49 +0530

    refactor: remove unnecessary code block

--- src/features/projects/singleProject/members/ProjectMemberDrawer.tsx
+++ src/features/projects/singleProject/members/ProjectMemberDrawer.tsx
@@ -21,8 +21,6 @@ const ProjectMemberDrawer = () => {
 
     const [form] = Form.useForm()
 
-    console.log(membersList)
-
     const onChange = (value: string) => {
         console.log(`selected ${value}`)
     }
