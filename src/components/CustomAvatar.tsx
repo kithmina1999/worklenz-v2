42197ad6800e95b9e12512930029a487f047c083
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-10-28 00:40:56 +0530

    feat: add custom avatar generator

---
+++ src/components/CustomAvatar.tsx
@@ -0,0 +1,18 @@
+import { Avatar } from 'antd'
+import React from 'react'
+import { avatarNamesMap } from '../shared/constants'
+
+const CustomAvatar = ({ avatarCharacter }: { avatarCharacter: string }) => {
+    return (
+        <Avatar
+            style={{
+                backgroundColor: avatarNamesMap[avatarCharacter],
+                verticalAlign: 'middle',
+            }}
+        >
+            {avatarCharacter}
+        </Avatar>
+    )
+}
+
+export default CustomAvatar
