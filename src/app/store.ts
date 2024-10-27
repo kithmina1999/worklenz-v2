85b07fd1a9284f936f61a83f3a2fed6dcaff0e6c
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-10-28 00:42:55 +0530

    refactor(store): update store with reducers

--- src/app/store.ts
+++ src/app/store.ts
@@ -16,6 +16,8 @@ import billingReducer from '../features/adminCenter/billing/billingSlice'
 import projectMemberReducer from '../features/projects/singleProject/members/projectMembersSlice'
 import categoriesReducer from '../features/settings/categories/categoriesSlice'
 import projectViewTaskListColumnsReducer from '../features/projects/singleProject/taskListColumns/taskColumnsSlice'
+import phaseReducer from '../features/projects/singleProject/phase/phaseSlice'
+import labelReducer from '../features/settings/label/labelSlice'
 
 export const store = configureStore({
     // there is error  occured with day js package when use with redux it shows a non-serializable value was detected
@@ -41,6 +43,8 @@ export const store = configureStore({
         projectMemberReducer: projectMemberReducer,
         categoriesReducer: categoriesReducer,
         projectViewTaskListColumnsReducer: projectViewTaskListColumnsReducer,
+        phaseReducer: phaseReducer,
+        labelReducer: labelReducer,
     },
 })
 
