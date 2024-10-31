86920b133f1db44c87f9f65f49ee00e23afc8453
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-10-31 10:32:26 +0530

    refactor: update categories

--- src/features/settings/categories/categoriesSlice.ts
+++ src/features/settings/categories/categoriesSlice.ts
@@ -6,18 +6,41 @@ type CategoriesState = {
 };
 
 const initialState: CategoriesState = {
-  categoriesList: [],
+  categoriesList: [
+    {
+      categoryId: 'category1',
+      categoryName: 'Development',
+      categoryColor: '#70a6f3',
+    },
+    {
+      categoryId: 'category2',
+      categoryName: 'Management',
+      categoryColor: '#ee87c5',
+    },
+    {
+      categoryId: 'category3',
+      categoryName: 'Testing',
+      categoryColor: '#bf4949',
+    },
+  ],
 };
 
 const categoriesSlice = createSlice({
   name: 'categoriesReducer',
   initialState,
   reducers: {
+    // action for add category
     addCategory: (state, action: PayloadAction<CategoryType>) => {
       state.categoriesList.push(action.payload);
+    },
+    // action for delete category
+    deleteCategory: (state, action: PayloadAction<string>) => {
+      state.categoriesList = state.categoriesList.filter(
+        (category) => category.categoryId !== action.payload
+      );
     },
   },
 });
 
-export const { addCategory } = categoriesSlice.actions;
+export const { addCategory, deleteCategory } = categoriesSlice.actions;
 export default categoriesSlice.reducer;
