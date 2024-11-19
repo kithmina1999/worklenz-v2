a73b31dacdd31c408b4cea0e2c1872aab3a2da12
Ravisa Dilanka <raveeshadilanka1999@gmail.com>
2024-11-19 18:29:21 +0530

    feat: Created task template table and json file and render data

--- src/pages/settings/taskTemplates/TaskTemplatesSettings.tsx
+++ src/pages/settings/taskTemplates/TaskTemplatesSettings.tsx
@@ -1,6 +1,9 @@
-import { Card, Table, TableProps } from 'antd';
+import { Card, Pagination, Table, TableProps } from 'antd';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { formatDistanceToNowStrict } from 'date-fns';
+import jsonData from './TaskTemplates.json';
+import './TaskTemplatesSettings.css';
 
 const TaskTemplatesSettings = () => {
   // localization
@@ -16,13 +19,16 @@ const TaskTemplatesSettings = () => {
     {
       key: 'created',
       title: t('createdColumn'),
-      dataIndex: 'created',
+      dataIndex: 'created_at',
+      render: (date: string) => formatDistanceToNowStrict(new Date(date), { addSuffix: true }),
     },
   ];
 
   return (
     <Card style={{ width: '100%' }}>
-      <Table columns={columns} />
+      <Table size='small' pagination={{size: 'small'}} columns={columns} dataSource={jsonData} rowKey="id" rowClassName={(_, index) =>
+          `no-border-row ${index % 2 === 0 ? '' : 'alternate-row-color'}`
+        }/>
     </Card>
   );
 };
