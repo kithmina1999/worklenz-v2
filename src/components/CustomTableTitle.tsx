b4c651eea60422e465a9d1320ba1390faaeb9d06
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-11-20 20:09:42 +0530

    refactor: update custom table title

--- src/components/CustomTableTitle.tsx
+++ src/components/CustomTableTitle.tsx
@@ -1,9 +1,28 @@
-import { Typography } from 'antd';
+import { Flex, Tooltip, Typography } from 'antd';
 import React from 'react';
+import { colors } from '../styles/colors';
+import { ExclamationCircleOutlined } from '@ant-design/icons';
 
 // this custom table title used when the typography font weigh 500 needed
-const CustomTableTitle = ({ title }: { title: string }) => {
-  return <Typography.Text style={{ fontWeight: 500 }}>{title}</Typography.Text>;
+const CustomTableTitle = ({
+  title,
+  tooltip,
+}: {
+  title: string;
+  tooltip?: string | null;
+}) => {
+  return (
+    <Flex gap={8} align="center">
+      <Typography.Text style={{ fontWeight: 500, textAlign: 'center' }}>
+        {title}
+      </Typography.Text>
+      {tooltip && (
+        <Tooltip title={tooltip} trigger={'hover'}>
+          <ExclamationCircleOutlined style={{ color: colors.skyBlue }} />
+        </Tooltip>
+      )}
+    </Flex>
+  );
 };
 
 export default CustomTableTitle;
