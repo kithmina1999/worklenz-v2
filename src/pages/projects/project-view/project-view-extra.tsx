14b2b233bced0ad983fe326dea4d8b046127edad
chamiakJ <chamika@ceydigital.com>
2024-12-18 06:12:49 +0530

    refactor: clean up socket event handling and improve member view logic in ProjectViewExtra component
    - Simplified the emitJoinOrLeave function by formatting the emitted object for clarity.
    - Removed commented-out code in handleMemberView and handleProjectUpdates for cleaner code.
    - Updated useEffect dependencies to remove unnecessary references, enhancing performance and readability.

--- src/pages/projects/project-view/project-view-extra.tsx
+++ src/pages/projects/project-view/project-view-extra.tsx
@@ -19,17 +19,15 @@ const ProjectViewExtra = () => {
   const LEAVE_TXT = 'leave';
 
   const emitJoinOrLeave = (type: string) => {
-    socket?.emit(SocketEvents.JOIN_OR_LEAVE_PROJECT_ROOM.toString(), {type, id: projectId});
+    socket?.emit(SocketEvents.JOIN_OR_LEAVE_PROJECT_ROOM.toString(), { type, id: projectId });
   };
 
   const handleMemberView = (res: any) => {
-    // dispatch(setActiveMembers(res));
-
+    dispatch(setActiveMembers(res));
   };
 
   const handleProjectUpdates = (res: any) => {
-        console.log(res);
-
+    console.log(res);
   };
 
   const handleProjectDataChange = (res: any) => {
@@ -48,7 +46,7 @@ const ProjectViewExtra = () => {
       socket?.removeListener(SocketEvents.PROJECT_UPDATES_AVAILABLE.toString(), handleProjectUpdates);
       socket?.removeListener(SocketEvents.PROJECT_DATA_CHANGE.toString(), handleProjectDataChange);
     };
-  }, [activeMembers, connected]);
+  }, [connected]);
 
   return (
     <div>
