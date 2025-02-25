dbf2a33ba2509b5b61fa65e3e309419a5ddcaeee
shancds <shancds1@gmail.com>
2025-02-25 15:36:42 +0530

    fix: Correct tasks_group_by value type in task mode change handler

--- src/pages/home/task-list/tasks-list.tsx
+++ src/pages/home/task-list/tasks-list.tsx
@@ -189,7 +189,7 @@ const TasksList: React.FC = React.memo(() => {
   );
 
   const handleTaskModeChange = (value: number) => {
-    dispatch(setHomeTasksConfig({ ...homeTasksConfig, tasks_group_by: +value }));
+    dispatch(setHomeTasksConfig({ ...homeTasksConfig, tasks_group_by: value }));
   };
 
   return (
